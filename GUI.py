2496c9e7c0c22064c614e5b1decf204dfc120217
Brian Aster <brianaster85@gmail.com>
2021-10-25 08:04:52 -0500

    Starting the GUI

---
+++ GUI.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# create window
+window = tk.Tk()
+
+# widgets
+title = tk.Label(text='Film Inventory Tracker')
+
+# packing widgets
+title.pack()
+
+# display the window
+window.mainloop()
